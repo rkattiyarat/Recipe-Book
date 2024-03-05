8202fc9ae49e1357a20bc1de20ead8c4323f8479
rose <aom_bnn@hotmail.com>
2024-03-04 19:24:41 -0500

    navigation tab using @output and EventEmitter

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,10 +1,16 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, EventEmitter, Output } from '@angular/core';
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html'
 })
 export class HeaderComponent implements OnInit {
-  constructor() { }
-  ngOnInit() {
+
+@Output() featureSelected = new EventEmitter<string>();
+
+constructor() { }
+ngOnInit() {}
+
+  onSelect(feature: string) {
+    this.featureSelected.emit(feature);
   }
 }
