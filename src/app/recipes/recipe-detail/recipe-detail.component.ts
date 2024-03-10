1450f1c431433b10a41b6ba17f92753e663a1076
rose <aom_bnn@hotmail.com>
2024-03-09 20:46:36 -0500

    passing ingredients to shopping-list using Services

--- src/app/recipes/recipe-detail/recipe-detail.component.ts
+++ src/app/recipes/recipe-detail/recipe-detail.component.ts
@@ -1,6 +1,8 @@
 import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { Recipe } from '../recipe.model';
 import { Ingredient } from '../../shared/ingredient.model';
+import { ShoppingListService } from '../../shopping-list/shopping-list.service';
+import { RecipeService} from '../recipe.service';
 
 @Component({
   selector: 'app-recipe-detail',
@@ -9,7 +11,14 @@ import { Ingredient } from '../../shared/ingredient.model';
 })
 export class RecipeDetailComponent {
 
+
   @Input() recipe: Recipe;
   @Input() ingredient: Ingredient;
+
+  constructor(private recipeService: RecipeService) { }
+
+  onAddToShoppingList() {
+    this.recipeService.addIngredientsToShoppingList(this.recipe.ingredients);
+  }
 
 }
--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,18 +1,25 @@
 import { Recipe } from "./recipe.model";
 import { EventEmitter, Injectable } from '@angular/core';
-
+import { Ingredient } from "../shared/ingredient.model";
+import { ShoppingListService } from "../shopping-list/shopping-list.service";
 @Injectable()
 export class RecipeService {
     recipeSelected = new EventEmitter<Recipe>();
     private recipes: Recipe[] = [
         new Recipe('A Test Recipe', 'This is simply a test', 
-        'https://iamafoodblog.b-cdn.net/wp-content/uploads/2020/11/korean-bbq-8064w.jpg'),
+        'https://iamafoodblog.b-cdn.net/wp-content/uploads/2020/11/korean-bbq-8064w.jpg',[new Ingredient('Pork', 15), new Ingredient('Veggies', 20)]),
         new Recipe('Another Test Recipe', 'This is also simply a test', 
-        'https://amandascookin.com/wp-content/uploads/2022/09/Korean-Fried-Chicken-RCSQ.jpg')
+        'https://amandascookin.com/wp-content/uploads/2022/09/Korean-Fried-Chicken-RCSQ.jpg', [new Ingredient('Chicken', 10), new Ingredient('Sasemes', 20)])
     ];
+
+    constructor(private shoppingListService: ShoppingListService) {}
 
 
     getRecipes() {
         return this.recipes.slice();
+    }
+
+    addIngredientsToShoppingList(ingredients: Ingredient[]) {
+        this.shoppingListService.addIngredients(ingredients);
     }
 }
